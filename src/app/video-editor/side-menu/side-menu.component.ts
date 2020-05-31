3f797c51cb0b7b85f82fdfa85a7b699c2d49a73e
zhuohongyu <hongyuzhuo@gmail.com>
2020-05-31 20:56:21 +0800

    添加侧边栏组件

---
+++ src/app/video-editor/side-menu/side-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vcr-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss']
+})
+export class SideMenuComponent implements OnInit {
+
+  activeMenu: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
