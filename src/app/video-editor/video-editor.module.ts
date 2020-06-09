e1e5811d647e507ec4884e95fb4ad31c6cc03c6e
zhuohongyu <hongyuzhuo@gmail.com>
2020-06-09 22:58:43 +0800

    new video-editor components

--- src/app/video-editor/video-editor.module.ts
+++ src/app/video-editor/video-editor.module.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { VideoEditorRoutingModule } from './video-editor-routing.module';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { VideoEditorComponent } from './video-editor.component';
+import { PreviewComponent } from './preview/preview.component';
+import { AspectRatioComponent } from './aspect-ratio/aspect-ratio.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ToolbarComponent } from './timeline/toolbar/toolbar.component';
 
 
 @NgModule({
-  declarations: [VideoEditorComponent, SideMenuComponent],
+  declarations: [
+    VideoEditorComponent,
+    SideMenuComponent,
+    PreviewComponent,
+    AspectRatioComponent,
+    TimelineComponent,
+    ToolbarComponent
+  ],
   imports: [
     CommonModule,
     VideoEditorRoutingModule
