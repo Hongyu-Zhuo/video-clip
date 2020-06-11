699a77e1b38350fe085e0dc52e71a7946c2e7a76
zhuohongyu <hongyuzhuo@gmail.com>
2020-06-11 21:25:24 +0800

    创建选择视频长宽比组件

---
+++ src/app/video-editor/preview/aspect-ratio-container/aspect-ratio-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AspectRatioContainerComponent } from './aspect-ratio-container.component';
+
+describe('AspectRatioContainerComponent', () => {
+  let component: AspectRatioContainerComponent;
+  let fixture: ComponentFixture<AspectRatioContainerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AspectRatioContainerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AspectRatioContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/video-editor/preview/aspect-ratio-container/aspect-ratio-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vc-aspect-ratio-container',
+  templateUrl: './aspect-ratio-container.component.html',
+  styleUrls: ['./aspect-ratio-container.component.scss']
+})
+export class AspectRatioContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
