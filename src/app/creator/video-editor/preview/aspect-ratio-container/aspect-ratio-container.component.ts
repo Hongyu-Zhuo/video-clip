74f3029ed18db6a190c7a3886009142d0e7cf178
zhuohongyu <hongyuzhuo@gmail.com>
2021-08-24 00:05:18 +0800

    chore(global) 迁移 TSLint 到 ESLlint

--- src/app/creator/video-editor/preview/aspect-ratio-container/aspect-ratio-container.component.ts
+++ src/app/creator/video-editor/preview/aspect-ratio-container/aspect-ratio-container.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'vc-aspect-ratio-container',
   templateUrl: './aspect-ratio-container.component.html',
   styleUrls: ['./aspect-ratio-container.component.scss']
 })
-export class AspectRatioContainerComponent implements OnInit {
+export class AspectRatioContainerComponent {
 
   @Input()
   aspectRatio = '16:9';
-  
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
